94af995415bef3ded589d48739dfb668f9b7c010
Almasyriqi <ikramalmasyriqi@gmail.com>
2022-05-18 18:32:46 +0700

    Update mapper

--- src/main/java/org/example/YoutubeMapper.java
+++ src/main/java/org/example/YoutubeMapper.java
@@ -1,2 +1,43 @@
-package org.example;public class YoutubeMapper {
+package org.example;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+import java.io.IOException;
+import static java.lang.String.format;
+public class YoutubeMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text>{
+
+    private Text channel_title = new Text();
+    private Text data = new Text();
+    @Override
+    public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter)
+            throws IOException {
+        String line = value.toString();
+
+        // Pecah dulu berdasarkan ','
+        String[] split = line.split(",");
+        System.out.println("Jumlah Split: " + split.length);
+
+        // cek apakah terdapat data view, like, dislike, dan comment
+        if(split.length > 10) {
+            String channel = split[3];
+            channel = channel.trim();
+            System.out.println("Channel title : " + channel);
+
+            // Cari data views, like, dislike, comment
+            String view = split[7];
+            String like = split[8];
+            String dislike = split[9];
+            String comment = split[10];
+
+            // Laporkan ke kolektor
+            this.channel_title.set(channel);
+            this.data.set(format("%s,%s,%s,%s", view, like, dislike, comment));
+            output.collect(this.channel_title, this.data);
+        }
+    }
 }
--- src/main/java/org/example/YoutubeReducer.java
+++ src/main/java/org/example/YoutubeReducer.java
@@ -1,2 +1,21 @@
-package org.example;public class YoutubeReducer {
+package org.example;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class YoutubeReducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+
+    @Override
+    public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output,
+                       Reporter reporter) throws IOException {
+        System.out.println("----------------");
+        System.out.println("Ini dari reducer");
+        System.out.println("Key: " + key.toString());
+        System.out.println("----------------");
+    }
 }
